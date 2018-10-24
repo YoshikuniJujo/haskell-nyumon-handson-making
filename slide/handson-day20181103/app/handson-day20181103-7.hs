ef8b8683a481941ddfcb8e47e37594718953c967
Yoshikuni Jujo <PAF011432[at]nifty.ne.jp>
2018-10-24 11:00:35 +0900

    Haskell入門ハンズオン! #4: Random: すこし

--- slide/handson-day20181103/app/handson-day20181103-7.hs
+++ slide/handson-day20181103/app/handson-day20181103-7.hs
@@ -13,7 +13,8 @@ main = runSlide version someSlide
 
 someSlide :: Slide
 someSlide = title :| [
-	prelude
+	prelude,
+	random1
 	]
 
 title :: Page
@@ -26,4 +27,19 @@ prelude = pageTitle "はじめに" :| [
 	itext 4 "そのために必要な追加の知識をここで学ぶ",
 	text "まずはHaskellにおいて、かつては、標準的だった",
 	itext 4 "乱数生成ライブラリについて学ぶ"
+	]
+
+random1 :: Page
+random1 = pageTitle "乱数生成" :| [
+	text "かつて標準的だったモジュールSystem.Randomは",
+	itext 4 "パッケージrandomに含まれている",
+	text "(このあたり、あとで確認する)",
+	itext 4 "> :module System.Random",
+	itext 4 "> randomIO :: IO Integer",
+	itext 4 "7070170094128852650",
+	itext 4 "> randomIO :: IO Double",
+	itext 4 "0.19573673642621692",
+	text "毎回ちがう乱数値を得るには",
+	itext 4 "「なんらかの状態変化」が必要なので",
+	itext 4 "randomIOは「入出力」になっている"
 	]
